a50b78ac6a2c3000d03cf20328fcb61627fe5914
Louis Payette <payette1996@gmail.com>
2023-03-14 14:08:13 -0400

    Initial commit

---
+++ class/classVoitureManager.php
@@ -0,0 +1,18 @@
+<?php
+    class VoitureManager
+    {
+        const SELECT_VOITURES = "SELECT * FROM tblvoiture ORDER BY idVoiture ASC";
+
+        private ?PDO $db;
+
+        public function __construct($db)
+        {
+            $this->db = $db;
+        }
+
+        public function getVoitures(PDO $db) : array
+        {
+            return $this->db->query(self::SELECT_VOITURES)->fetchAll();
+        }
+    }
+?>
